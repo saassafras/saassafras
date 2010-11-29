7aea3af727be26e27496b3b5a7199b776e693af7
Aaric Pittman <aaricpittman@gmail.com>
2010-11-28 21:54:53 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saassafras::Application.config.secret_token = 'ec70ffcb56e4180b753078d5f15d8ee624e01b6c94036210b7b97c133f1fa7798f88e828254b732880136683fa074b08e231ab1b3e79f7bb5e64b9e35f3e75a4'
